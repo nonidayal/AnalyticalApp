3e1daecd905cca5b160a70a5258cae216fcff270
Salman Mohammad <salman@innovapte.com>
2022-11-11 05:09:47 +0000

    bug fix

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,6 +7,7 @@ sap.ui.define(
   
       return BaseController.extend("sample.project.salesdocumentmanagement.controller.App", {
         onInit() {
+          // dvgdvsgdsvg
         }
       });
     }
